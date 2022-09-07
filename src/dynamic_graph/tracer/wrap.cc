3f097ec1f0260f0c98cfe48f892d1534249f4992
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-09-07 14:33:04 +0200

    Rebase from 'release/melodic/dynamic-graph-python'

--- src/dynamic_graph/tracer/wrap.cc
+++ src/dynamic_graph/tracer/wrap.cc
@@ -1,10 +1,11 @@
-#include "dynamic-graph/python/module.hh"
-
 #include <dynamic-graph/tracer.h>
+
+#include "dynamic-graph/python/module.hh"
 
 BOOST_PYTHON_MODULE(wrap) {
   using dynamicgraph::Tracer;
 
   bp::import("dynamic_graph");
-  dynamicgraph::python::exposeEntity<Tracer>().def("addSignal", &Tracer::addSignalToTrace);
+  dynamicgraph::python::exposeEntity<Tracer>().def("addSignal",
+                                                   &Tracer::addSignalToTrace);
 }
