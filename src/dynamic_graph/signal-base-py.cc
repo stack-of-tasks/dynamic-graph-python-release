41ae45a40c1e8df5e9a31a4f91865f808ed07c7e
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-09-07 14:33:07 +0200

    Rebase from 'debian/melodic/dynamic-graph-python'

--- src/dynamic_graph/signal-base-py.cc
+++ src/dynamic_graph/signal-base-py.cc
@@ -1,21 +1,19 @@
 // Copyright 2010, Florent Lamiraux, Thomas Moulard, LAAS-CNRS.
 
+#include <dynamic-graph/linear-algebra.h>
+#include <dynamic-graph/signal-base.h>
+#include <dynamic-graph/signal-ptr.h>
+#include <dynamic-graph/signal-time-dependent.h>
+#include <dynamic-graph/signal.h>
+#include <dynamic-graph/value.h>
+
+#include <boost/python.hpp>
 #include <iostream>
 #include <sstream>
 
-#include <boost/python.hpp>
-
-#include "dynamic-graph/python/signal.hh"
-
-#include <dynamic-graph/signal-base.h>
-#include <dynamic-graph/signal.h>
-#include <dynamic-graph/signal-ptr.h>
-#include <dynamic-graph/signal-time-dependent.h>
-#include <dynamic-graph/linear-algebra.h>
-#include <dynamic-graph/value.h>
-
 #include "dynamic-graph/python/dynamic-graph-py.hh"
 #include "dynamic-graph/python/signal-wrapper.hh"
+#include "dynamic-graph/python/signal.hh"
 
 using dynamicgraph::SignalBase;
 
@@ -43,52 +41,68 @@ template <typename Time>
 void exposeSignalBase(const char* name) {
   typedef SignalBase<Time> S_t;
   bp::class_<S_t, boost::noncopyable>(name, bp::no_init)
-      .add_property("time", bp::make_function(&S_t::getTime, bp::return_value_policy<bp::copy_const_reference>()),
+      .add_property("time",
+                    bp::make_function(
+                        &S_t::getTime,
+                        bp::return_value_policy<bp::copy_const_reference>()),
                     &S_t::setTime)
-      .add_property("name", bp::make_function(&S_t::getName, bp::return_value_policy<bp::copy_const_reference>()))
+      .add_property("name",
+                    bp::make_function(
+                        &S_t::getName,
+                        bp::return_value_policy<bp::copy_const_reference>()))
 
-      .def("getName", &S_t::getName, bp::return_value_policy<bp::copy_const_reference>())
-      .def("getClassName",
-           +[](const S_t& s) -> std::string {
-             std::string ret;
-             s.getClassName(ret);
-             return ret;
-           })
+      .def("getName", &S_t::getName,
+           bp::return_value_policy<bp::copy_const_reference>())
+      .def(
+          "getClassName",
+          +[](const S_t& s) -> std::string {
+            std::string ret;
+            s.getClassName(ret);
+            return ret;
+          })
 
       .def("plug", &S_t::plug, "Plug the signal to another signal")
       .def("unplug", &S_t::unplug, "Unplug the signal")
       .def("isPlugged", &S_t::isPlugged, "Whether the signal is plugged")
-      .def("getPlugged", &S_t::getPluged, bp::return_value_policy<bp::reference_existing_object>(),
+      .def("getPlugged", &S_t::getPluged,
+           bp::return_value_policy<bp::reference_existing_object>(),
            "To which signal the signal is plugged")
 
       .def("recompute", &S_t::recompute, "Recompute the signal at given time")
 
-      .def("__str__",
-           +[](const S_t& s) -> std::string {
-             std::ostringstream oss;
-             s.display(oss);
-             return oss.str();
-           })
-      .def("displayDependencies",
-           +[](const S_t& s, int time) -> std::string {
-             std::ostringstream oss;
-             s.displayDependencies(oss, time);
-             return oss.str();
-           },
-           "Print the signal dependencies in a string");
+      .def(
+          "__str__",
+          +[](const S_t& s) -> std::string {
+            std::ostringstream oss;
+            s.display(oss);
+            return oss.str();
+          })
+      .def(
+          "displayDependencies",
+          +[](const S_t& s, int time) -> std::string {
+            std::ostringstream oss;
+            s.displayDependencies(oss, time);
+            return oss.str();
+          },
+          "Print the signal dependencies in a string");
 }
 
 template <>
 auto exposeSignal<MatrixHomogeneous, time_type>(const std::string& name) {
   typedef Signal<MatrixHomogeneous, time_type> S_t;
-  bp::class_<S_t, bp::bases<SignalBase<time_type> >, boost::noncopyable> obj(name.c_str(), bp::init<std::string>());
-  obj.add_property("value", +[](const S_t& signal) -> Matrix4 { return signal.accessCopy().matrix(); },
-                   +[](S_t& signal, const Matrix4& v) {
-                     // TODO it isn't hard to support pinocchio::SE3 type here.
-                     // However, this adds a dependency to pinocchio.
-                     signal.setConstant(MatrixHomogeneous(v));
-                   },
-                   "the signal value.");
+  bp::class_<S_t, bp::bases<SignalBase<time_type> >, boost::noncopyable> obj(
+      name.c_str(), bp::init<std::string>());
+  obj.add_property(
+      "value",
+      +[](const S_t& signal) -> Matrix4 {
+        return signal.accessCopy().matrix();
+      },
+      +[](S_t& signal, const Matrix4& v) {
+        // TODO it isn't hard to support pinocchio::SE3 type here.
+        // However, this adds a dependency to pinocchio.
+        signal.setConstant(MatrixHomogeneous(v));
+      },
+      "the signal value.");
   return obj;
 }
 
@@ -115,7 +129,8 @@ void exposeSignals() {
 namespace signalBase {
 
 template <class T>
-SignalWrapper<T, int>* createSignalWrapperTpl(const char* name, bp::object o, std::string& error) {
+SignalWrapper<T, int>* createSignalWrapperTpl(const char* name, bp::object o,
+                                              std::string& error) {
   typedef SignalWrapper<T, int> SignalWrapper_t;
   if (!SignalWrapper_t::checkCallable(o, error)) {
     return NULL;
@@ -138,7 +153,8 @@ PythonSignalContainer* getPythonSignalContainer() {
 /**
    \brief Create an instance of SignalWrapper
 */
-SignalBase<int>* createSignalWrapper(const char* name, const char* type, bp::object object) {
+SignalBase<int>* createSignalWrapper(const char* name, const char* type,
+                                     bp::object object) {
   PythonSignalContainer* psc = getPythonSignalContainer();
   if (psc == NULL) return NULL;
 
