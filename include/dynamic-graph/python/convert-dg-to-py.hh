dfb1bd00eae09f407d5d20429764fa2867989cf3
Guilhem Saurel <guilhem.saurel@laas.fr>
2022-09-07 14:32:52 +0200

    Rebase from 'upstream'

--- include/dynamic-graph/python/convert-dg-to-py.hh
+++ include/dynamic-graph/python/convert-dg-to-py.hh
@@ -1,15 +1,16 @@
 // Copyright 2010, Florent Lamiraux, Thomas Moulard, LAAS-CNRS.
 
-#include <boost/python.hpp>
-
 #include <dynamic-graph/linear-algebra.h>
 #include <dynamic-graph/value.h>
+
+#include <boost/python.hpp>
 
 namespace dynamicgraph {
 namespace python {
 namespace convert {
 
-command::Value toValue(boost::python::object o, const command::Value::Type& type);
+command::Value toValue(boost::python::object o,
+                       const command::Value::Type& type);
 boost::python::object fromValue(const command::Value& value);
 
 }  // namespace convert
